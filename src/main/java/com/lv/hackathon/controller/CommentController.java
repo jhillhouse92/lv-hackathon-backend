03e100704561e89cd24148bdd090eadd4f3a4379
Joseph Hillhouse <jhillhouse@longviewinc.com>
2015-05-22 12:21:34 -0400

    added save comment service with correct REST methods

--- src/main/java/com/lv/hackathon/controller/CommentController.java
+++ src/main/java/com/lv/hackathon/controller/CommentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +22,12 @@ public class CommentController {
 	@Autowired
 	private CommentService commentService;
 	
-	@RequestMapping(value = "/{postId}/comments")
+	@RequestMapping(value = "/{postId}/comments", method = RequestMethod.GET)
 	public List<Comment> getCommentsForPost(@PathVariable String postId){
 		return commentService.getComments(postId);
 	}
 	
-	@RequestMapping(value = "/{postId}/comment")
+	@RequestMapping(value = "/{postId}/comment", method = RequestMethod.PUT)
 	@ResponseBody
 	public HttpResponseMessage addComment(@RequestBody Comment comment){
 		if(commentService.save(comment)){
